2a29549350e7b04dcb99b88f50fbc4ea1b6b02e1
Glitch (glitchypastepen) <none>
2020-06-03 11:33:53 +0000

    🚗🐴 Checkpoint ./public/script.js:3168504/1254 ./views/editor.html:3168504/581:5152800/641

--- public/script.js
+++ public/script.js
@@ -1,8 +1,18 @@
+function html() {
+  editor.setSession("html");
+}
+
+function js() {
+  editor.setSession("js");
+}
+
+function css() {
+  editor.setSession("css");
+}
 
 function dismiss() {
   document.getElementById("status").style.display = "none";
 }
-
 
 // fetch("/projectname", {
 //   headers: { "Content-Type": "application/json" }
