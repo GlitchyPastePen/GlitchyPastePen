76cfc5fdd6dc73d452ab95d796e5e196e862f10d
Glitch (glitchypastepen) <none>
2020-06-06 12:48:54 +0000

    🐠😂 Checkpoint ./public/style.css:3168504/167 ./routes.js:3168504/24826

--- routes.js
+++ routes.js
@@ -11,11 +11,13 @@ const {
   animals
 } = require("unique-names-generator");
 const config = require("./config");
-const Endb = require('endb');
+const Endb = require("endb");
 var contributor = new Endb("sqlite://contributor.db");
 
 // Endpoints
 module.exports.run = ({ app, user, project } = {}) => {
+  const clientID = process.env.CLIENT_ID;
+  const clientSecret = process.env.CLIENT_SECRET;
   app.all("*", (request, response, next) => {
     // protocol check, if http, redirect to https
     if (request.get("X-Forwarded-Proto").indexOf("https") != -1) {
@@ -23,6 +25,64 @@ module.exports.run = ({ app, user, project } = {}) => {
     } else {
       response.redirect("https://" + request.hostname + request.url);
     }
+  });
+
+  app.get("/login/github", (req, res) => {
+    res.redirect(
+      `https://github.com/login/oauth/authorize?client_id=${process.env.CLIENT_ID}`
+    );
+  });
+
+  async function getAccessToken(code, client_id, client_secret) {
+    const request = await fetch("https://github.com/login/oauth/access_token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        client_id,
+        client_secret,
+        code
+      })
+    });
+    const text = await request.text();
+    console.log("RESPONSE!!!");
+    console.log(text);
+    const params = new URLSearchParams(text);
+    return params.get("access_token");
+  }
+
+  async function fetchGitHubUser(token) {
+    const request = await fetch("https://api.github.com/user", {
+      headers: {
+        Authorization: "token " + token
+      }
+    });
+    return await request.json();
+  }
+
+  app.get("/login/github/callback", async (req, res) => {
+    const code = req.query.code;
+    const access_token = await getAccessToken(code, clientID, clientSecret);
+    console.log(access_token);
+    const user = await fetchGitHubUser(access_token);
+    if (user) {
+      req.session.access_token = access_token;
+      req.session.github = user;
+      req.session.githubId = user.id;
+      req.session.username = req.session.github.login;
+      req.session.loggedin = true;
+      res.redirect("/dashboard");
+    } else {
+      res.send("Login did not succeed!");
+    }
+  });
+
+  app.get("/logout", (req, res) => {
+    // if (req.session) req.session = null;
+    req.session.destroy(function(err) {
+      res.redirect("/");
+    });
   });
 
   app.get("/", async (request, response) => {
